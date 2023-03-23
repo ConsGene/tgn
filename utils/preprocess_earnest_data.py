384f170737c98be2e3317a4e167da2dde63a3196
manyuanQ <manyuan0525@gmail.com>
2023-03-22 23:32:29 -0400

    Update preprocess_earnest_data.py
    Able to take example csv file and generate corresponding npy documents

--- utils/preprocess_earnest_data.py
+++ utils/preprocess_earnest_data.py
@@ -13,12 +13,12 @@ def preprocess(data_name):
 
   raw_df = pd.read_csv(data_name)
   
-  raw_df['ts'] = raw_df['optimized_date'].apply(pd.to_datetime).astype(int) // 10**9 // WEEK_IN_SECS
-  df = raw_df.groupby(['member_id','merchant', 'ts']).agg({'transaction_amount':'sum', 'category':'first', 'member_home_state':'first'}).reset_index()
+  raw_df['ts'] = raw_df['optimized_date'].apply(pd.to_datetime).astype('int64') // 10**9 // (1*WEEK_IN_SECS)
+  df = raw_df.groupby(['member_id','merchant_format_name', 'ts']).agg({'transaction_amount':'sum', 'category':'first', 'member_home_state':'first'}).reset_index()
   
   df = df.sort_values('ts')
   result_df = pd.DataFrame({'u': df['member_id'].astype('category').cat.codes.tolist(),
-                            'i': df['merchant'].astype('category').cat.codes.tolist(),
+                            'i': df['merchant_format_name'].astype('category').cat.codes.tolist(),
                             'ts': df['ts'].tolist(),
                             'label': df['transaction_amount'].tolist(),
                             'idx': df.index.values.tolist()})
@@ -57,11 +57,17 @@ def run(data_name, bipartite=True):
   df, u_feat, i_feat = preprocess(PATH)
   new_df = reindex(df, bipartite)
 
-  # TODO: empty node feature?
+  ## TODO: empty node feature?
+  max_idx = max(new_df.u.max(), new_df.i.max())
+  node_feat_dim = max(u_feat.shape[1], i_feat.shape[1])
+  if node_feat_dim % 2 != 0:
+    node_feat_dim += 1
+      
+  feat = np.zeros((len(new_df), node_feat_dim))
   empty = np.zeros(feat.shape[1])[np.newaxis, :]
   feat = np.vstack([empty, feat])
 
-  max_idx = max(new_df.u.max(), new_df.i.max())
+  #max_idx = max(new_df.u.max(), new_df.i.max())
   rand_feat = np.zeros((max_idx + 1, feat.shape[1]))
 
   new_df.to_csv(OUT_DF)
@@ -72,7 +78,7 @@ def run(data_name, bipartite=True):
 
 parser = argparse.ArgumentParser('Interface for TGN data preprocessing')
 parser.add_argument('--data', type=str, help='Dataset name (eg. wikipedia or reddit)',
-                    default='5K')
+                    default='u2k_i200')
 parser.add_argument('--bipartite', action='store_true', help='Whether the graph is bipartite')
 
 args = parser.parse_args()
